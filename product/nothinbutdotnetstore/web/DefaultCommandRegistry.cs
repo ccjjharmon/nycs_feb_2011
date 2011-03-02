a3caba1bf75539f6bdcce6367038e103fa259ba6
paulsh <mbgr6@yahoo.com>
2011-03-01 19:35:24 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
@@ -1,12 +1,29 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.web
 {
     public class DefaultCommandRegistry : CommandRegistry
     {
+
+        private IEnumerable<RequestCommand> requestCommands;
+        public RequestCommand DefaultCommand { get; set; }
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> requestCommands)
+        {
+            this.requestCommands = requestCommands;
+        }
+
         public RequestCommand get_the_command_that_can_handle(Request request)
         {
-            throw new NotImplementedException();
+            foreach (var requestCommand in requestCommands)
+            {
+                if (requestCommand.can_handle(request))
+                {
+                    return requestCommand;
+                }
+            }
+
+            return DefaultCommand;
         }
     }
 }
