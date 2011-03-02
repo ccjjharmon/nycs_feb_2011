8f885a6aeb1f3909ec649ec7abd66913e3596f3c
ccjjharmon <ccjjharmon@gmail.com>
2011-03-01 20:17:51 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
@@ -7,15 +7,17 @@ namespace nothinbutdotnetstore.web
     public class DefaultCommandRegistry : CommandRegistry
     {
         IEnumerable<RequestCommand> all_commands;
+        private MissingRequestCommandFactory missingcmdfactory;
 
-        public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands)
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> list, MissingRequestCommandFactory missingcommandfactory)
         {
+            this.missingcmdfactory = missingcommandfactory;
             this.all_commands = all_commands;
         }
 
         public RequestCommand get_the_command_that_can_handle(Request request)
         {
-            return all_commands.First(x => x.can_handle(request));
+            return all_commands.FirstOrDefault(x => x.can_handle(request)) ?? this.missingcmdfactory.Invoke();
         }
     }
 }
