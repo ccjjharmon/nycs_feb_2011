c02f322219d012a6c654aa1a74245bd7b19b1656
Rick Hodder <rickhodder@msn.com>
2011-03-01 20:09:22 -0500

    Pushing new changes

--- product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
@@ -7,15 +7,19 @@ namespace nothinbutdotnetstore.web
     public class DefaultCommandRegistry : CommandRegistry
     {
         IEnumerable<RequestCommand> all_commands;
+        private MissingRequestCommandFactory specialCase;
 
-        public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands)
+      
+        public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands, MissingRequestCommandFactory specialcase)
         {
             this.all_commands = all_commands;
+            this.specialCase = specialcase;
         }
 
         public RequestCommand get_the_command_that_can_handle(Request request)
         {
-            return all_commands.First(x => x.can_handle(request));
+            RequestCommand result = all_commands.FirstOrDefault(x => x.can_handle(request));
+            return result == null ? specialCase.Invoke() : result;
         }
     }
 }
