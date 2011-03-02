0b0b02a73e0f83ea1d4e382ed9ce1a70d02d8a09
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2011-03-01 20:22:46 -0500

    Updated the DefaultCommandRegistry

--- product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
+++ product/nothinbutdotnetstore/web/DefaultCommandRegistry.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,19 +6,18 @@ namespace nothinbutdotnetstore.web
     public class DefaultCommandRegistry : CommandRegistry
     {
         IEnumerable<RequestCommand> all_commands;
-        private MissingRequestCommandFactory specialCase;
+        MissingRequestCommandFactory special_case;
 
-      
         public DefaultCommandRegistry(IEnumerable<RequestCommand> all_commands, MissingRequestCommandFactory specialcase)
         {
             this.all_commands = all_commands;
-            this.specialCase = specialcase;
+            this.special_case = specialcase;
         }
 
         public RequestCommand get_the_command_that_can_handle(Request request)
         {
-            RequestCommand result = all_commands.FirstOrDefault(x => x.can_handle(request));
-            return result == null ? specialCase.Invoke() : result;
+            return all_commands.FirstOrDefault(x => x.can_handle(request))
+                ?? special_case();
         }
     }
 }
